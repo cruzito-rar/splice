dc4cd55f9b8f859cd5ab00913a6990e3fdedeec0
David Cruz <dcruzer92@gmail.com>
2023-08-27 23:18:27 -0600

    Logout button has added

--- public/src/components/Logout.jsx
+++ public/src/components/Logout.jsx
@@ -1,13 +1,37 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { BiPowerOff } from "react-icons/bi";
 
 const Logout = () => {
+  const navigate = useNavigate();
+  const handleClick = async () => {
+    localStorage.clear();
+    navigate("/login");
+  }
   return (
     <>
-      <div>
-        Logout
-      </div>
+      <Button onClick={handleClick}>
+        <BiPowerOff />
+      </Button>
     </>
   )
 }
 
 export default Logout;
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: .5rem;
+  border-radius: .5rem;
+  background-color: #05B0FF;
+  border: none;
+  cursor: pointer;
+
+  svg {
+    font-size: 1.3rem;
+    color: #FFFFFF;
+  }
+`;
