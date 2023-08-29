352bafdfd8d79d14714313db8c1486321c9aaa10
David Cruz <dcruzer92@gmail.com>
2023-08-28 19:12:08 -0600

    Message Model created, now messageRoute is created

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,31 +2,31 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: {
-    type: String,
-    required: true,
-    minlength: 4,
-    maxlength: 20,
-    unique: true,
+    type : String,
+    required : true,
+    minlength : 4,
+    maxlength : 20,
+    unique : true,
   },
   email: {
-    type: String,
-    required: true,
-    maxlength: 50,
-    unique: true,
+    type : String,
+    required : true,
+    maxlength : 50,
+    unique : true
   },
   password: {
-    type: String,
-    required: true,
-    minlength: 6,
+    type : String,
+    required : true,
+    minlength : 6
   },
   isAvatarImageSet: {
-    type: Boolean,
-    default: false,
+    type : Boolean,
+    default : false
   },
   avatarImage: {
-    type: String,
-    default: "",
-  },
+    type : String,
+    default : ""
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
